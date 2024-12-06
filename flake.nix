fba50b5bf7a3fc27781046a2fe98500e43b1f69c
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-12-06 21:57:05 +0800

    build(flake): initialize flake framework

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Fr4nk1in's NixVim Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    # nixvim,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        # nixvimLib = nixvim.lib.${system};
+        # nixvim' = nixvim.legacyPackages.${system};
+      in {
+        checks = {
+          preCommit = inputs.git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              statix.enable = true;
+              deadnix.enable = true;
+              commitizen.enable = true;
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.preCommit) shellHook;
+          buildInputs = self.checks.${system}.preCommit.enabledPackages;
+        };
+      }
+    );
+}
