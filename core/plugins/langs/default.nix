998af0b5853baea62e8f2f74b8113b6c5d0ca50b
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-12-10 02:47:35 +0800

    refactor(lang): move go/llvm/lua/markdown/rust language support into extras
    BREAKING CHANGE:

--- core/plugins/langs/default.nix
+++ core/plugins/langs/default.nix
@@ -3,14 +3,9 @@
     ./bash.nix
     ./clangd.nix
     ./cmake.nix
-    ./go.nix
     ./json.nix
-    ./llvm.nix
-    ./lua.nix
-    ./markdown.nix
     ./nix.nix
     ./python.nix
-    ./rust.nix
     ./yaml.nix
   ];
 }
--- extra/langs/default.nix
+++ extra/langs/default.nix
@@ -1,9 +1,14 @@
 {...}: {
   imports = [
     ./dotfile.nix
+    ./go.nix
     ./kotlin.nix
     ./latex.nix
+    ./llvm.nix
     ./ltex.nix
+    ./lua.nix
+    ./markdown.nix
+    ./rust.nix
     ./typst.nix
   ];
 }
