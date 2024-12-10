6fbb13d46608d8a482b1a0688c843d1c0eef271b
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-12-11 05:04:20 +0800

    fix(lazy): disable lazy loading for auto session

--- core/plugins/editor/session.nix
+++ core/plugins/editor/session.nix
@@ -2,11 +2,6 @@ _: {
   plugins.auto-session = {
     enable = true;
 
-    lazyLoad = {
-      enable = true;
-      settings.event = "DeferredUIEnter";
-    };
-
     settings = {
       auto_restore = false;
       cwd_change_handling = true;
